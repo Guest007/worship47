8299bcb835faf0f5e2f528300f04add6155fad79
Guest007 <trash4@yandex.ru>
2023-07-02 17:06:20 +0300

    feature: start to setup github CI (env variables in secrets)

--- src/worship47/settings/vars.py
+++ src/worship47/settings/vars.py
@@ -5,7 +5,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = bool(os.getenv("DEBUG", default=0))
 # 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
 # For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS").split(" ")
+_ALLOWED_HOSTS_STR = os.getenv("DJANGO_ALLOWED_HOSTS", None)
+ALLOWED_HOSTS = _ALLOWED_HOSTS_STR.split(" ") if _ALLOWED_HOSTS_STR else ["*"]
 ROOT_URLCONF = "worship47.urls"
 WSGI_APPLICATION = "worship47.wsgi.application"
 SITE_ID = 1
